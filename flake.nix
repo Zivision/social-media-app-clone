24d5a6ec89a0b840064e5c9e95458d8331fd8181
Zivision <zcastro703@gmail.com>
2026-04-26 14:53:27 -0500

    feat: basic changes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Clojure Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Uncomment for either runtime
+          clojure
+          # babashka
+
+          # Tools for emacs
+          # Formatter
+          cljfmt
+          clj-kondo
+
+          # Leiningen
+          leiningen
+
+          # JDK Version
+          jdk25
+
+        ];
+        env = {
+          # Point Clojure to local project
+          JAVA_OPTS = "-Xmx2g -XX:+UseG1GC";
+          CLJ_CONFIG = "${builtins.getEnv "HOME"}/.clojure";
+        };
+        shellHook = ''
+          echo "Clojure Ready!"
+        '';
+      };
+    };
+}
