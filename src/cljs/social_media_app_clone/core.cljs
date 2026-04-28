6c2bec58e98764fa3b68fb866cc602b8a03a7eb2
Zivision <zcastro703@gmail.com>
2026-04-28 08:47:24 -0500

    feat: frontend functional

--- src/cljs/social_media_app_clone/core.cljs
+++ src/cljs/social_media_app_clone/core.cljs
@@ -1,4 +1,19 @@
-(ns social-media-app-clone.core)
+(ns social-media-app-clone.core
+  (:require [reagent.core :as r]
+            [reagent.dom.client :as rdomc]
+            [ajax.core :refer [GET]]))
 
-(defn init []
-  (println "Hello World"))
+(defn greeting [message]
+  [:h1 message])
+
+(defn app []
+  [:div
+   [greeting "Hello World! This is the Social Media App Clone"]])
+
+;; Delay create-root call until app is mounted,
+;; this is only because this ns is already part of the demosite build.
+;; Delay is not usually needed for regular apps.
+(defonce root (delay (rdomc/create-root (.getElementById js/document "app"))))
+
+(defn ^:export init []
+  (rdomc/render @root [app]))
